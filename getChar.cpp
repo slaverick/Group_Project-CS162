68028cfaa76dbce4209c41965609505212bff5cc
slaverick <scott.laverick@gmail.com>
2017-02-21 22:52:00 -0600

    Update getChar.cpp

--- getChar.cpp
+++ getChar.cpp
@@ -6,14 +6,13 @@
 char getChar()
 {
 	char c;
-	std::cout << "Please enter the tool you would like to choose: r for rock, p for paper, or s for scissors:" << std::endl;
 	std::cin >> c;
 
 	while ((c < 97)||(c > 122))
 	{
-		std::cout << "Please enter a lowercase alphabet character." << std::endl;
+		std::cout << "Invalid selection. Please re-enter." << std::endl;
 		std::cin >> c;
 	}
-	std::cout << "You entered " << c << " ." << std::endl;
+//	std::cout << "You entered " << c << " ." << std::endl;
 	return c;
 }
