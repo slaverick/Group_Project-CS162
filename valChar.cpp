a52a719ee5fd3a00b314c132f0dda98e14c84370
slaverick <scott.laverick@gmail.com>
2017-02-21 22:52:50 -0600

    Update valChar.cpp

--- valChar.cpp
+++ valChar.cpp
@@ -5,7 +5,7 @@
 char valChar(char &c) {
 	while (c != A && c != B && c != C)
 	{
-		std::cout << "You entered " << c << "." << std::endl;
+//		std::cout << "You entered " << c << "." << std::endl;
 		std::cout << "Please enter one of the following characters: " << A << ", " << B << ", or " << C << "." << std::endl;
 		c = getChar();
 	}
