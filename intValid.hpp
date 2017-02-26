b90b0843a94be4c2a69a80cf6a583a321d9114ff
benninkc <benninkc@oregonstate.edu>
2017-02-26 01:13:09 -0500

    updated for errant double input

--- intValid.hpp
+++ intValid.hpp
@@ -1,16 +1,19 @@
 /*************************************************************
-** Program: Project 3 - Fantasy Combat
-** Author: Scott Laverick
-** Date: 2/10/2017
-** Description:
+** Program: CS 162 Group Project Rock Paper Scissors
+** Author: Group 8
+** Date: 2/26/2017
+** Description: function to handle input validation.
+** Specification file file.
 **************************************************************/
 
 #ifndef INTEGERVALIDATOR_HPP
 #define INTEGERVALIDATOR_HPP
 
 #include <string>
+#include <sstream>
+#include <iostream>
 
-int intValid(std::string valIn);
+int intValid();
 
 
 #endif
